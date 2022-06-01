055c439a17d690ccc08ea54534ca4871c3a504f8
dHinagas <hinagasrodriguez@gmail.com>
2022-06-01 12:21:02 -0400

    added new endpoint products #8

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import provider from './provider.routes.js'
+import products from './products.routes.js'
 
 const router = Router()
 
 router.use('/api/provider', provider)
+router.use('/api/products', products)
 
 export default router
