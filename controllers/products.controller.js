1f302e0adfef2720575c16e063e2d3baaf7cf9c0
dHinagas <hinagasrodriguez@gmail.com>
2022-06-07 17:21:41 -0400

    new provider attr added for products

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,6 +1,7 @@
 import Product from "../models/product.model.js"
 import Category from "../models/category.model.js"
 import Stock from "../models/stock.model.js"
+import Provider from "../models/provider.model.js"
 
 export const getProducts = async (req, res) => {
   try {
@@ -9,8 +10,9 @@ export const getProducts = async (req, res) => {
     let rawProducts = await Promise.all(
       products.map(async product => {
       const category = await Category.findById(product.category)
+      const provider = await Provider.findById(product.provider)
       const stock = await Stock.findById(product.id)
-      return {...product, category: category.shift(), stock: stock.shift()}
+      return {...product, category: category.shift(), stock: stock.shift(), provider: provider.shift()}
     }))
 
     return res.json({ products: rawProducts })
@@ -21,19 +23,45 @@ export const getProducts = async (req, res) => {
 
 export const createProduct = async (req, res) => {
   const product = req.body
+  try {
+    const categories = await Category.find()
+    const category = categories.find(category => category.codigo === product.category) ?? false
 
-  const categories = await Category.find()
-  const category = categories.find(category => category.codigo === product.category) ?? false
+    if (!category) return res.status(404).json({ message: 'Category product not found' })
 
-  if (!category) return res.status(404).json({ message: 'Category product not found' })
+    const providers = await Provider.find()
+    const provider = providers.find(provider => provider.rif == product.provider) ?? false
+
+    if (!provider) return res.status(404).json({ message: "Provider Not Found" })
   
-  const savedStock = await Stock.save(product.stock)
-  product.category = category.id
-  product.stock = savedStock.insertId
+    const savedStock = await Stock.save(product.stock)
+    product.category = category.id
+    product.stock = savedStock.insertId
   
-  const savedProduct = await Product.save(product)
-  return res.json({ message: 'New product added', product: savedProduct.insertId })
+    const savedProduct = await Product.save({...product, provider: provider.id})
+    return res.json({ message: 'New product added', product: savedProduct.insertId })
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
 }
 
 export const editProduct = () => {}
-export const getProduct = () => {}
+
+export const getProduct = async (req, res) => {
+  try {
+    const { id } = req.params
+    const product = await Product.findById(Number(id))
+    
+    if (!product) return res.status(404).json({ message: "Provider Not Found" });
+
+    const rawProduct = product.shift()
+    
+    const provider = await Provider.findById(rawProduct.provider)
+    const stock = await Stock.findById(rawProduct.stock)
+    const category = await Category.findById(rawProduct.category)
+    
+    return res.json({ product: {...rawProduct, category: category.shift(), stock: stock.shift(), provider: provider.shift() } })
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
+}
--- models/product.model.js
+++ models/product.model.js
@@ -2,9 +2,9 @@ import connection from "../utils/mysql.js";
 
 const Product = {}
 
-Product.save = ({ name, category, stock, description, actived = 1, price = 0.00 }) => new Promise((resolve, reject) => {
-  const post = [name, category, stock, description, actived, price]
-  connection.query('INSERT INTO product (name, category, stock, description, actived, price) VALUES (?, ?, ?, ?, ?, ?)', post, (err, results) => {
+Product.save = ({ name, category, stock, description, actived = 1, price = 0.00, provider }) => new Promise((resolve, reject) => {
+  const post = [name, category, stock, description, actived, price, provider]
+  connection.query('INSERT INTO product (name, category, stock, description, actived, price, provider) VALUES (?, ?, ?, ?, ?, ?, ?)', post, (err, results) => {
     if (err) reject(err)
 
     resolve(results)
