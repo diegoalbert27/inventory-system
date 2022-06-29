eb64bffbc8c6747f08f715bc5bca6ce72f537afc
dHinagas <hinagasrodriguez@gmail.com>
2022-06-29 16:03:42 -0400

    new pool connection for production

--- utils/mysql.js
+++ utils/mysql.js
@@ -1,16 +1,42 @@
 import mysql from 'mysql'
+import { config } from 'dotenv'
+config()
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'inventory_system'
-})
+let connection = null
 
-connection.connect(err => {
-  if (err) console.log('Error connecting: ' + err.stack)
+const { NODE_ENV } = process.env
 
-  console.log('Connected to DB ' + connection.threadId)
-})
+if (NODE_ENV === 'production') {
+  connection = mysql.createPool({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME,
+    ssl : {
+      rejectUnauthorized: false
+    }
+  });
+
+  connection.getConnection(function(err, conn) {
+    if (err) console.log('Error connecting: ' + err.stack)
+
+    console.log('Connected to DB ' + conn.threadId)
+  });
+}
+
+if (NODE_ENV === 'development') {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'inventory_system'
+  })
+
+  connection.connect(err => {
+    if (err) console.log('Error connecting: ' + err.stack)
+
+    console.log('Connected to DB ' + connection.threadId)
+  })
+}
 
 export default connection
